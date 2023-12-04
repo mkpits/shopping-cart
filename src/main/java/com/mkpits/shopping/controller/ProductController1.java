3188c4c5bc22157ac36873c139f625448218158d
Bharat Gayki <bhaaratgayki@gmail.com>
2023-12-04 11:41:11 +0530

    ResolveTheIssuesOfCodeReleatedToControlerPackage

--- src/main/java/com/mkpits/shopping/controller/ProductController1.java
+++ src/main/java/com/mkpits/shopping/controller/ProductController1.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+
+
 @Controller
 public class ProductController1 {
     @Autowired
